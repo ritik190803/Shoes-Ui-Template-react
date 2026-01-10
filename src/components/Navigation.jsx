9e0fa18c50c45c6a4ecc673cb198d183e98594d3
ritik190803 <ritiksingh1915@gmail.com>
2026-01-10 23:39:39 +0530

    Initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation=()=>{
+return (
+  <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>MENU</li>
+          <li>LOCATION</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+);
+};
+export default Navigation;
